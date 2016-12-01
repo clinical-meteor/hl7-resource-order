e6667df981d59c17c9a999e391c8649def1b7a51
Abigail Watson <awatson1978@gmail.com>
2016-12-01 12:37:47 -0600

    bump deps

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-order',
-  version: '1.1.1',
+  version: '1.1.2',
   summary: 'HL7 FHIR Resource - Order',
   git: 'https://github.com/clinical-meteor/hl7-resource-order',
   documentation: 'README.md'
@@ -17,7 +17,7 @@ Package.onUse(function (api) {
   api.use('prime8consulting:meteor-oauth2-server@0.0.2');
 
   api.use('clinical:base-model@1.3.5');
-  api.use('clinical:hl7-resource-datatypes@0.5.6');
+  api.use('clinical:hl7-resource-datatypes@0.6.0');
 
   api.addFiles('lib/hl7-resource-order.js', ['client', 'server']);
   api.addFiles('server/rest.js', 'server');
